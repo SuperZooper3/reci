78059a12d7728e116c8cec19e03be7175395516e
Daniel Zhang <z331317@gmail.com>
2025-05-20 21:15:35 -0400

    Finishes milestone 0

---
+++ backend/src/db.ts
@@ -0,0 +1,20 @@
+import { Client } from 'pg';
+
+//Might want to see if there's a better/more reusable way to connect to the db.
+export const client = new Client({
+  host: 'localhost',
+  port: 5432,
+  user: 'postgres',
+  password:'postgres',
+  database: 'testdb',
+});
+
+export async function initDb() {
+    await client.connect();
+    await client.query<void>(
+      'CREATE TABLE IF NOT EXISTS users(name text);'
+    );
+    await client.query<void>(
+      `INSERT INTO users values('Bill'), ('Russell');`
+    );
+}
