9766c7e0fd1112d5e940228298d04e8b5198e098
SuperZooper3 <54122151+SuperZooper3@users.noreply.github.com>
2025-06-06 00:29:21 -0400

    nit spacing

--- backend/sql/init.sql
+++ backend/sql/init.sql
@@ -12,6 +12,7 @@ CREATE TABLE IF NOT EXISTS Accounts(
     password VARCHAR NOT NULL,
     PRIMARY KEY(id)
 );
+
 CREATE TABLE IF NOT EXISTS Recipe(
     id INT,
     title VARCHAR NOT NULL,
@@ -21,6 +22,7 @@ CREATE TABLE IF NOT EXISTS Recipe(
     PRIMARY KEY(id),
     FOREIGN KEY(author_id) REFERENCES Accounts(id)
 );
+
 CREATE TABLE IF NOT EXISTS Review(
     id INT,
     created_at DATE NOT NULL,
@@ -32,12 +34,14 @@ CREATE TABLE IF NOT EXISTS Review(
     FOREIGN KEY(recipe_id) REFERENCES Recipe(id),
     FOREIGN KEY(account_id) REFERENCES Accounts(id)
 );
+
 CREATE TABLE IF NOT EXISTS Follower(
     id INT NOT NULL,
     follower_id INT NOT NULL,
     followee_id INT NOT NULL,
     PRIMARY KEY(id)
 );
+
 CREATE TABLE IF NOT EXISTS SavedRecipe(
     account_id INT NOT NULL,
     recipe_id INT NOT NULL,
