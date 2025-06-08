8d0f50d38c3a93df60904caeec08af43f925fe7b
Dominik Ritz <dom_ritz@icloud.com>
2025-06-08 01:00:36 -0400

    feat: add getAccount (#93)

--- backend/src/routes/accountRoutes.ts
+++ backend/src/routes/accountRoutes.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
-import { getAccounts } from '../controllers/accountController.js';
+import { getAccount, getAccounts } from '../controllers/accountController.js';
 
 const accountRouter = Router();
 
 accountRouter.get('/', getAccounts);
+accountRouter.get('/:id', getAccount);
 
 export default accountRouter;
