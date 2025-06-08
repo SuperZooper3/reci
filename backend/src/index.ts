5a45c6bdf9c444312039f6d53e65c813e55a3954
Dominik Ritz <dom_ritz@icloud.com>
2025-06-08 13:59:57 -0400

    feat: add review endpoint

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,7 +4,8 @@ import cookieParser from 'cookie-parser';
  
 import accountRouter from './routes/accountRoutes.js';
 import recipeRouter from './routes/recipeRoutes.js';
-
+import reviewRouter from './routes/reviewRoutes.js';
+import cors from 'cors';
 import { initDb } from './db.js';
 
 initDb();
@@ -17,6 +18,7 @@ app.use(cookieParser());
 
 app.use('/api/accounts', accountRouter);
 app.use('/api/recipes', recipeRouter);
+app.use('/api/reviews', reviewRouter);
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
