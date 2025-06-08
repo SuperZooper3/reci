b9c4d2dada9af0dc777bd31aa82387476158d22f
Dominik Ritz <dom_ritz@icloud.com>
2025-06-07 23:29:30 -0400

    feat: add account types

---
+++ backend/src/types/account.ts
@@ -0,0 +1,13 @@
+export interface Account {
+    id: number;
+    displayName: string;
+    username: string;
+    createdAt: Date;
+}
+
+export interface AccountRow {
+    id: number;
+    display_name: string;
+    username: string;
+    created_at: Date;
+}
