9e269bd17e31e6183e3957cb44cdebee2987958b
SuperZooper3 <54122151+SuperZooper3@users.noreply.github.com>
2025-06-11 19:09:49 -0400

    feat: add saving to all endpoints

--- backend/src/models/accountModel.ts
+++ backend/src/models/accountModel.ts
@@ -14,22 +14,27 @@ export async function getAccount(id: number): Promise<AccountInfo | null>
  {
   const getAccountSQL = await loadSQL('getAccount.sql');
   const { rows } = await client.query<{ id: number, username: string, display_name: string, created_at: Date }>(getAccountSQL, [id]);
+  saveQueryResult("getAccount", rows);
   return rows[0] ?? null;
 }
 
 export async function createAccount(display_name: string, username: string, hashedPassword: string): Promise<{ id: number, display_name: string, username: string }> {
   const createAccountSQL = await loadSQL('createAccount.sql');
   const { rows } = await client.query<{ id: number, display_name: string, username: string }>(createAccountSQL, [display_name, username, hashedPassword]);
+  saveQueryResult("createAccount", rows);
   return { ...rows[0] };
 }
 
 export async function deleteAccount(id: number): Promise<void> {
   const deleteAccountSQL = await loadSQL('deleteAccount.sql');
-  await client.query(deleteAccountSQL, [id]);
+  const { rows } = await client.query(deleteAccountSQL, [id]);
+  saveQueryResult("deleteAccount", rows);
+  return;
 }
 
 export async function getAccountPassword(id: number): Promise<{ password: string }[]> {
   const getPasswordSQL = await loadSQL('getAccountPassword.sql');
   const { rows } = await client.query<{ password: string }>(getPasswordSQL, [id]);
+  saveQueryResult("getAccountPassword", rows);
   return rows;
 }
--- backend/src/utils/saveQueryResult.ts
+++ backend/src/utils/saveQueryResult.ts
@@ -1,10 +1,20 @@
 import * as fs from 'fs';
 
-const resultPath = "/sql/results/"
+const resultPath = "sql/results/"
 
 export function saveQueryResult(queryName: string, result: object[]) {
-    const headerLine = Object.keys(result[0]).join(",");
-    const contents = result.map((v) => {Object.values(v).join(",")}).join("\n"); // the ordering may be sus here
+    fs.mkdirSync(resultPath, { recursive: true });
+
+    const keys = Object.keys(result[0]);
+    const headerLine = keys.join(",");
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const contents = result.map((v: Record<string, any>) => {
+        const values = [];
+        for (const k of keys) {
+            values.push(String(v[k]));
+        }
+        return values.join(",");
+    }).join("\n"); // ordering here might be sus
     const output = `${headerLine}\n${contents}\n`;
     const path = `${resultPath}${queryName}.out`;
     fs.writeFile(path, output, (err) => {
