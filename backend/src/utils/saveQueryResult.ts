2647f3d1f3d135c819a3ef1241233979c0533c1f
SuperZooper3 <54122151+SuperZooper3@users.noreply.github.com>
2025-06-11 19:43:13 -0400

    fix: nits to make better saving of queries

--- backend/src/utils/saveQueryResult.ts
+++ backend/src/utils/saveQueryResult.ts
@@ -3,25 +3,30 @@ import * as fs from 'fs';
 const resultPath = "sql/results/"
 
 export function saveQueryResult(queryName: string, result: object[]) {
-    fs.mkdirSync(resultPath, { recursive: true });
+    try {
+        fs.mkdirSync(resultPath, { recursive: true });
 
-    const keys = Object.keys(result[0]);
-    const headerLine = keys.join(",");
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const contents = result.map((v: Record<string, any>) => {
-        const values = [];
-        for (const k of keys) {
-            values.push(String(v[k]));
-        }
-        return values.join(",");
-    }).join("\n"); // ordering here might be sus
-    const output = `${headerLine}\n${contents}\n`;
-    const path = `${resultPath}${queryName}.out`;
-    fs.writeFile(path, output, (err) => {
-        if (err) {
-            console.error('Error writing to file:',err);
-        } else {
-            console.log(`${path} written succesfully`);
-        }
-    });
+        const keys = Object.keys(result[0]);
+        const headerLine = keys.join(",");
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        const contents = result.map((v: Record<string, any>) => {
+            const values = [];
+            for (const k of keys) {
+                values.push(String(v[k]).replace(/\n/g," "));
+            }
+            console.log(values);
+            return values.join(",");
+        }).join("\n"); // ordering here might be sus
+        const output = `${headerLine}\n${contents}\n`;
+        const path = `${resultPath}${queryName}.out`;
+        fs.writeFile(path, output, (err) => {
+            if (err) {
+                console.error('Error writing to file:',err);
+            } else {
+                console.log(`${path} written succesfully`);
+            }
+        });
+    } catch (err) {
+        console.error('Something went wrong when saving the query result', err)
+    }
 }
