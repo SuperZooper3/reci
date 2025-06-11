fc50e90f67af5dc1954efb1d4f76b34ea34fc52d
Dominik Ritz <dom_ritz@icloud.com>
2025-06-10 20:19:38 -0400

    fix: update SQL query for fetching reviews by recipe ID

--- backend/src/models/reviewModel.ts
+++ backend/src/models/reviewModel.ts
@@ -2,7 +2,7 @@ import { client } from '../db.js';
 import { loadSQL } from '../utils/sqlLoader.js';
 
 export async function getReviewsByRecipeId(recipe_id: number): Promise<{ id: number, created_at: Date, description: string, rating: number, recipe_id: number, account_id: number }[]> {
-    const getReviewsByRecipeIdSQL = await loadSQL('getReviewsByRecipeId.sql')
+    const getReviewsByRecipeIdSQL = await loadSQL('getReview.sql')
     const { rows } = await client.query(getReviewsByRecipeIdSQL, [recipe_id]);
     return rows;
 };
