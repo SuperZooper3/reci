6a2c16898e522bdd543e0672ef00652e1d14ca27
Dominik Ritz <dom_ritz@icloud.com>
2025-07-05 15:21:32 -0400

    feat: add endpoint to remove following relationship

--- backend/src/routes/accountRoutes.ts
+++ backend/src/routes/accountRoutes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { getAccount, deleteAccount, getAccounts, getAccountMe, createAccount, getAccountsFollowing, getAccountsFollowers, addAccountFollowing } from '../controllers/accountController.js';
+import { getAccount, deleteAccount, getAccounts, getAccountMe, createAccount, getAccountsFollowing, getAccountsFollowers, addAccountFollowing, deleteAccountFollow } from '../controllers/accountController.js';
 
 const accountRouter = Router();
 
@@ -8,6 +8,7 @@ accountRouter.get('/me', getAccountMe);
 accountRouter.get('/:id', getAccount);
 accountRouter.post('/create', createAccount);
 accountRouter.delete('/me/delete', deleteAccount);
+accountRouter.delete('/me/follow', deleteAccountFollow);
 accountRouter.get('/:id/following', getAccountsFollowing);
 accountRouter.get('/:id/followers', getAccountsFollowers);
 accountRouter.post('/:id/following/:followingAccountId', addAccountFollowing);
