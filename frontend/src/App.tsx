45605fabf719d449646b636c5aa4cbdaf9e56dde
MarcoLoDico <marcolodico1@gmail.com>
2025-07-05 18:15:26 -0400

    Added sidebar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,27 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import HomePage from './pages/HomePage'
-import SearchPage from './pages/SearchPage'
-import FollowingPage from './pages/FollowingPage'
-import RecipePage from './pages/RecipePage';
+import { Routes, Route } from "react-router-dom"
+import { SidebarProvider } from "@/components/ui/sidebar"
+import { AppSidebar } from "@/components/app-sidebar"
+import HomePage from "./pages/HomePage"
+import SearchPage from "./pages/SearchPage"
+import FollowingPage from "./pages/FollowingPage"
+import RecipePage from "./pages/RecipePage"
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/recipes" element={<SearchPage />} />
-      <Route path="/account/:id/following" element={<FollowingPage />} />
-      <Route path="/recipe/:id" element={<RecipePage />} />
-    </Routes>
-  );
+    <SidebarProvider>
+      <div className="flex">
+        <AppSidebar />
+        <main className="flex-1 p-4">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/recipes" element={<SearchPage />} />
+            <Route path="/account/:id/following" element={<FollowingPage />} />
+            <Route path="/recipe/:id" element={<RecipePage />} />
+          </Routes>
+        </main>
+      </div>
+    </SidebarProvider>
+  )
 }
+
 export default App
