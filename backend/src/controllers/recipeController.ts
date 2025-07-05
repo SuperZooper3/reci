625d2b0ad9fe9fe286c3aa9a362d3c099d9a97fc
Dominik Ritz <dom_ritz@icloud.com>
2025-07-05 19:26:11 -0400

    feat: add endpoint to save a recipe for a user

--- backend/src/controllers/recipeController.ts
+++ backend/src/controllers/recipeController.ts
@@ -61,3 +61,20 @@ export const getSavedRecipesByAccountId = async (req: Request, res: Response) =>
     res.status(500).json({ message: 'Internal server error' });
   }
 }
+
+export const addSavedRecipe = async (req: Request, res: Response) => {
+  const jwt = req.cookies.authToken;
+  if (!jwt) {
+    res.status(400).json({ message: 'Missing JWT cookie' });
+    return
+  }
+  const { id } = auth.verifyAndReadJWT(jwt);
+  const recipe_id = req.body.recipe_id;
+  try{
+    const accountSavedRecipes = await recipeModel.addSavedRecipe(id, recipe_id);
+    res.json(accountSavedRecipes);
+  } catch (error) {
+    console.error('Error adding account\'s saved recipes', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+};
