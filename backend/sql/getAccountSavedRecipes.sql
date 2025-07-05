c2e5d7056857af967646696e57055dac0ee3f794
SuperZooper3 <54122151+SuperZooper3@users.noreply.github.com>
2025-07-05 16:59:58 -0400

    change to a left join to preserve recipies with no accounts when looking at saved

--- backend/sql/getAccountSavedRecipes.sql
+++ backend/sql/getAccountSavedRecipes.sql
@@ -1,6 +1,6 @@
 SELECT r.id, r.title, r.body, r.created_at, r.author_id, a.display_name, a.username, sr.saved_at
 FROM SavedRecipe sr
 JOIN Recipe r ON sr.recipe_id = r.id
-JOIN Account a on r.author_id = a.id
+LEFT JOIN Account a on r.author_id = a.id
 WHERE sr.account_id = $1
 ORDER BY sr.saved_at DESC;
