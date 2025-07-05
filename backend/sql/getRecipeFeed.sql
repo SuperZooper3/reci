7191c1ca935023c3d24bb8e242dd29fd00a9edd1
SuperZooper3 <54122151+SuperZooper3@users.noreply.github.com>
2025-07-05 11:44:58 -0400

    feat: write recipe feed query

---
+++ backend/sql/getRecipeFeed.sql
@@ -0,0 +1,8 @@
+SELECT * 
+FROM Review 
+JOIN Recipe ON Review.recipe_id = Recipe.id
+JOIN Account ON Review.account_id = Account.id
+LEFT JOIN Follower ON 
+    Follower.followee_id = Review.account_id AND 
+    Follower.follower_id = $1
+ORDER BY followee_id DESC, created_at DESC;
