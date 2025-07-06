9c95f58c2f87c86a1860b5db442dede8b39877ee
Daniel Zhang <danzhang83@gmail.com>
2025-07-05 22:48:51 -0400

    Create feed page (#190)
    * Adds feed

---
+++ frontend/src/components/recipeCard.tsx
@@ -0,0 +1,24 @@
+import ReactMarkdown from "react-markdown";
+import type { Recipe } from "../../../shared-types";
+
+type RecipeProps = {
+  recipe: Recipe
+};
+
+export default function RecipeCard({ recipe }: RecipeProps) {
+  return (
+    <div key={recipe.id} className="recipe-card p-4 mb-4 border rounded-lg shadow">
+        <h2>{recipe.title}</h2>
+        <div className="recipe-date text-sm text-gray-500 mb-2">
+            Created: {new Date(recipe.created_at).toLocaleDateString()}
+        </div>
+        <div className="recipe-body prose max-w-none">
+            <ReactMarkdown>{recipe.body}</ReactMarkdown>
+        </div>
+        <div className="recipe-meta mt-4 text-xs text-gray-400 flex gap-4">
+            <span>ID: {recipe.id}</span>
+            {recipe.author_id && <span>Author ID: {recipe.author_id}</span>}
+        </div>
+    </div>
+  );
+}
--- frontend/src/pages/SearchPage.tsx
+++ frontend/src/pages/SearchPage.tsx
@@ -1,10 +1,10 @@
 import { useEffect, useState } from 'react';
 import { Button } from "@/components/ui/button";  
 import { Input } from '@/components/ui/input';
-import ReactMarkdown from 'react-markdown';
 import RecipeModal from '../components/recipeModal';
 import { filterRecipes } from '@/services/recipeService';
 import type { Recipe } from '../../../shared-types';
+import RecipeCard from '@/components/recipeCard';
 
 function SearchPage() {  
   const [recipes, setRecipes] = useState<Recipe[]>([]);
@@ -38,24 +38,14 @@ function SearchPage() {
           <Button onClick={handleFilterRecipes}>Search recipes</Button>
         </div>
 
-        <div className="recipes-container">
+        <div>
           {recipes.length > 0 ? (
             <>
             <h2>Displaying top 10 results:</h2>
-              <ul className="recipe-list">
+              <ul>
                 {recipes.map((recipe) => (
-                  <li key={recipe.id} className="recipe-card p-4 mb-4 border rounded-lg shadow">
-                    <h2>{recipe.title}</h2>
-                    <div className="recipe-date text-sm text-gray-500 mb-2">
-                      Created: {new Date(recipe.created_at).toLocaleDateString()}
-                    </div>
-                    <div className="recipe-body prose max-w-none">
-                      <ReactMarkdown>{recipe.body}</ReactMarkdown>
-                    </div>
-                    <div className="recipe-meta mt-4 text-xs text-gray-400 flex gap-4">
-                      <span>ID: {recipe.id}</span>
-                      {recipe.author_id && <span>Author ID: {recipe.author_id}</span>}
-                    </div>
+                  <li key={recipe.id}>
+                    <RecipeCard recipe={recipe}/>
                   </li>
                 ))}
               </ul>
