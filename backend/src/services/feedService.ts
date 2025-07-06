49c27fe4b75ec7a26a8f6d5b3b080f9611bd36cf
SuperZooper3 <54122151+SuperZooper3@users.noreply.github.com>
2025-07-05 20:08:03 -0400

    feat: add images to feed

---
+++ backend/src/services/feedService.ts
@@ -0,0 +1,15 @@
+import * as feedModel from '../models/feedModel.js';
+import { fetchImagesForReviews } from './reviewImageService.js';
+
+export async function getFeedService(userID: number | null) {
+    let reviews = null;
+    if (userID) {
+        reviews = await feedModel.getUserFeed(userID);
+    } else {
+        reviews = await feedModel.getAnonFeed();
+    }
+
+    reviews = await fetchImagesForReviews(reviews);
+    console.log(reviews);
+    return reviews;
+} 
--- shared-types/feedTypes.ts
+++ shared-types/feedTypes.ts
@@ -1,10 +1,5 @@
-export type FeedEntry = {
-    description: string,
-    created_at: string,
-    rating: number,
-    recipe_id: number,
-    title: string,
-    account_id: number,
-    display_name: string,
-    username: string
+import { Review } from "./reviewTypes";
+
+export type FeedEntry = Review & {
+    display_name: string;
 };
