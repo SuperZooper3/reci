6b9703b407950e9f0615a80fded7f7b8dae44f16
Dominik Ritz <dom_ritz@icloud.com>
2025-07-07 13:31:47 -0400

    fix: update SQL query for deleting review images to use USING clause

--- backend/sql/deleteReviewImage.sql
+++ backend/sql/deleteReviewImage.sql
@@ -1,3 +1,6 @@
 DELETE FROM ReviewImage
-JOIN Review ON ReviewImage.review_id = Review.id
-WHERE ReviewImage.id = $1 AND ReviewImage.review_id = $2 AND Review.account_id = $3;
+USING Review
+WHERE ReviewImage.review_id = Review.id
+AND ReviewImage.id = $1
+AND ReviewImage.review_id = $2
+AND Review.account_id = $3;
