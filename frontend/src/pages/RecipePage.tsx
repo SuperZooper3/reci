18f8fb56b548b6f1b8e853cec7e205b186dfdd59
Daniel Zhang <danzhang83@gmail.com>
2025-07-12 00:11:55 -0400

    Add clickthrough links (#234)
    * Adds clickthrough links
    ---------
    
    Co-authored-by: Daniel Zhang <z331317@gmail.com>

--- frontend/src/pages/RecipePage.tsx
+++ frontend/src/pages/RecipePage.tsx
@@ -3,7 +3,7 @@ import ReactMarkdown from 'react-markdown';
 import RecipeModal from '../components/recipeModal';
 import { getRecipe } from '@/services/recipeService';
 import type { Review, Recipe } from '../../../shared-types';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { getRecipeRatings } from '@/services/reviewService';
 import ReviewCard from '@/components/reviewCard';
 import { getColorBasedOnRating } from '@/utils/ratingUtils';
@@ -32,14 +32,13 @@ function RecipePage() {
     return <>Loading...</>;
   }
 
-  //TODO: click through to recipe uploader account/ review accounts?
   return (
     <>
       <div className="m-8 w-full flex !flex-row gap-6 items-center">
         <div>
           <h1 className="text-3xl">{recipe!.title}</h1>
           <span>
-            By {recipe.display_name ?? "Unknown user"} on {' '}
+            By {recipe.display_name != null ? <Link to={`/account/${recipe.author_id}`} className="hover:underline">{recipe.display_name}</Link> : "Unknown user"} on {' '}
             {new Date(recipe!.created_at).toLocaleDateString()}
           </span>
         </div>
@@ -52,12 +51,12 @@ function RecipePage() {
         <ReactMarkdown>{recipe!.body}</ReactMarkdown>
       </div> 
       <div>
-        <div className="text-3xl">
+        <div className="text-3xl mt-15">
           Cooks
         </div>
         {reviews.length > 0 ? (
           <>
-            <h2>Displaying top 10 reviews:</h2>
+            <h2>Displaying top 10 cooks:</h2>
             <ul>
               {reviews.map((review) => (
                 <li key={review.id}>
