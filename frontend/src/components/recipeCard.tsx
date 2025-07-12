8d37d0f5c6f357dab912d5d9dbd75553b1dee99e
MinIcecream <63301542+MinIcecream@users.noreply.github.com>
2025-07-11 20:44:03 -0400

    Adds clickthrough links

--- frontend/src/components/recipeCard.tsx
+++ frontend/src/components/recipeCard.tsx
@@ -1,6 +1,7 @@
 import ReactMarkdown from "react-markdown";
 import type { Recipe } from "../../../shared-types";
 import { getColorBasedOnRating } from "@/utils/ratingUtils";
+import { Link } from "react-router-dom";
 
 type RecipeProps = {
   recipe: Recipe
@@ -11,18 +12,15 @@ export default function RecipeCard({ recipe }: RecipeProps) {
     <div key={recipe.id} className="recipe-card p-4 mb-4 border rounded-lg shadow">
       <div className="flex justify-between">
         <div>
-          <h2>{recipe.title}</h2>
+          <Link to={`/recipe/${recipe.id}`} className="hover:underline text-2xl">{recipe.title}</Link>
           <div className="recipe-date text-sm text-gray-500 mb-2">
-              Created by {recipe.display_name} on {new Date(recipe.created_at).toLocaleDateString()}
+              Created by {recipe.display_name != null ? <Link to={`/recipe/${recipe.author_id}`} className="hover:underline">{recipe.display_name}</Link> : "unknown user"} on {new Date(recipe.created_at).toLocaleDateString()}
           </div>
         </div>
         <div className={`flex justify-center items-center w-10 h-10 border-2 rounded-full ${getColorBasedOnRating(recipe.avg)}`}> {recipe.avg ?? "?"}</div>
       </div>
       <div className="recipe-body prose max-w-none">
           <ReactMarkdown>{recipe.body}</ReactMarkdown>
-      </div>
-      <div className="recipe-meta mt-4 text-xs text-gray-400 flex gap-4">
-          <span>ID: {recipe.id}</span>
       </div>
     </div>
   );
--- frontend/src/components/reviewCard.tsx
+++ frontend/src/components/reviewCard.tsx
@@ -7,29 +7,24 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel"
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 type ReviewProps<T extends Review> = {
   review: T
 };
 
 export default function ReviewCard<T extends Review>({ review }: ReviewProps<T>) {
-  const navigate = useNavigate();
-
-  const navigateToUser = (id: number) => {
-    navigate(`/account/${id}`);
-  };
-
   return (
     <div className="p-4 mb-4 border rounded-lg shadow flex flex-col">
       <div className="flex w-full flex-col">
         <div className="text-xl flex flex-row w-full gap-6">
           <span>
-            <span onClick={navigateToUser(review.account_id)}>{review.username} </span>
-            {getMoodBasedOnRating(review.rating)} {"title" in review ? (review.title as string) : "this"}
+            <Link to={`/account/${review.account_id}`} className="hover:underline">{review.username}</Link>
+             {` ${getMoodBasedOnRating(review.rating)} `}
+            {"title" in review ? <Link to={`/recipe/${review.recipe_id}`} className="hover:underline">{review.title as string} </Link> : "this"}
           </span>
         </div>
-        <div className="w-full flex flex-row !justify-between mb-2"> 
+        <div className="w-full flex flex-row !justify-between mb-2">
           <span>{new Date(review.created_at).toLocaleDateString()}</span>
           <div className={`flex justify-center items-center w-10 h-10 border-2 rounded-full ${getColorBasedOnRating(review.rating)}`}> {review.rating}</div>
         </div>
